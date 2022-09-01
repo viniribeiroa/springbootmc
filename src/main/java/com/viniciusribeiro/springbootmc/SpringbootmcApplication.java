72ae7443b097c788af876fab2eb4f4bd9adb1928
Vinicius Alves Ribeiro <viniribeiroa@gmail.com>
2022-09-01 15:21:49 -0300

    Classe Produto e relacionamento

--- src/main/java/com/viniciusribeiro/springbootmc/SpringbootmcApplication.java
+++ src/main/java/com/viniciusribeiro/springbootmc/SpringbootmcApplication.java
@@ -2,17 +2,24 @@ package com.viniciusribeiro.springbootmc;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.viniciusribeiro.springbootmc.domain.Categoria;
+import com.viniciusribeiro.springbootmc.domain.Produto;
 import com.viniciusribeiro.springbootmc.repositories.CategoriaRepository;
+import com.viniciusribeiro.springbootmc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringbootmcApplication implements CommandLineRunner{
 	
+	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootmcApplication.class, args);
@@ -24,7 +31,21 @@ public class SpringbootmcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"informatica");
 		Categoria cat2 = new Categoria(null, "escritorio");
 		
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "impressora", 800.00);
+		Produto p3 = new Produto(null, "monitor", 550.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+			
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 	}
 
 }
