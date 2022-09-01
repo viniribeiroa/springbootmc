8ca9e8ad81c5b34d0f241bf1e4c0b689b446b4ea
Vinicius Alves Ribeiro <viniribeiroa@gmail.com>
2022-09-01 13:28:46 -0300

    Operação de instanciação

--- src/main/java/com/viniciusribeiro/springbootmc/SpringbootmcApplication.java
+++ src/main/java/com/viniciusribeiro/springbootmc/SpringbootmcApplication.java
@@ -1,13 +1,30 @@
 package com.viniciusribeiro.springbootmc;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.viniciusribeiro.springbootmc.domain.Categoria;
+import com.viniciusribeiro.springbootmc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringbootmcApplication {
+public class SpringbootmcApplication implements CommandLineRunner{
+	
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootmcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
