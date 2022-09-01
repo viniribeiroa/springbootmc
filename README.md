4e3e03c55e2628b8961de20633b08292fa5e25b9
Vinicius Alves Ribeiro <viniribeiroa@gmail.com>
2022-09-01 10:36:48 -0300

    Dependencia JPA e configuração

---
+++ README.md
@@ -0,0 +1,3 @@
+# springbootmc
+ Modelo Conceitual UML do Projeto
+![uml](https://user-images.githubusercontent.com/100477025/187909477-8781567b-7d17-464b-88b8-32bf5de250ae.PNG)
---
+++ src/test/java/com/viniciusribeiro/springbootmc/SpringbootmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusribeiro.springbootmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
