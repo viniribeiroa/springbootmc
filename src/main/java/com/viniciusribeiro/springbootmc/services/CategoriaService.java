9d6b0a29fcace08818116e47d29fd2d30ecb0c46
Vinicius Alves Ribeiro <viniribeiroa@gmail.com>
2022-09-02 14:12:59 -0300

    REST Exception Categoria

--- src/main/java/com/viniciusribeiro/springbootmc/services/CategoriaService.java
+++ src/main/java/com/viniciusribeiro/springbootmc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.viniciusribeiro.springbootmc.domain.Categoria;
 import com.viniciusribeiro.springbootmc.repositories.CategoriaRepository;
+import com.viniciusribeiro.springbootmc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,8 @@ public class CategoriaService {
 	public Categoria bucar(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+			return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ",Tipo: " + Categoria.class.getName()));
 	}
 
 }
